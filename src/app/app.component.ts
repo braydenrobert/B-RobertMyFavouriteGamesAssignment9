c18b28cd7b66b801a1f449d34cfbd2116e78afbf
braydenrobert <W0740867@myscc.ca>
2023-04-13 12:09:36 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ContentCardComponent } from 'src/app/content-card/content-card.component';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-content-list></app-content-list>',
+})
+export class AppComponent {
+  title = 'B-Robert-MyFavouriteGames';
+}
+
